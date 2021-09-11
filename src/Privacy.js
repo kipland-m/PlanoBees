118ed4b92133acfc5e6ec3658f0d3473331f1717
Kipland Melton <kiplandmelton@gmail.com>
2021-09-11 16:55:30 -0500

    privacy hyperlink and content put in place

--- src/Privacy.js
+++ src/Privacy.js
@@ -4,24 +4,11 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h2>About Us</h2>
-        <p>This is the about page.</p>
+        <h2>Privacy</h2>
+        <p>This is the privacy page.</p>
  
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.
-        Lorem ipsum dolor sit amet, 
-        consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-        ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-        reprehenderit in voluptate velit esse cillum dolore eu 
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-     	sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-        ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-        reprehenderit in voluptate velit esse cillum dolore eu 
-        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p>This website collects your email address, and will foward any
+        news/updates via a newsletter to the selected email address.
         </p>
       </div>
     );
